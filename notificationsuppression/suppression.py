d1eb597844c8318cbf2d09673ab3568fc0f5a4f0
priyansh <priyansh.pm@gmail.com>
2022-01-18 17:36:03 +0800

    Added pylinted code

--- notificationsuppression/suppression.py
+++ notificationsuppression/suppression.py
@@ -1,54 +1,53 @@
-import time
-import os
-import yaml
 import datetime
-from random import randrange
+import yaml
 
-def creation():
-    with open(r'time.yaml') as rfile:
-        violation_list = yaml.load(rfile, Loader=yaml.FullLoader)
-    try:
-        exceptionhandler = len(violation_list)
-    except Exception as e:
-        dict_file = {'Time' : 3600}
-        with open(r'time.yaml', 'w') as wfile:
-            documents = yaml.dump(dict_file, wfile)
-        
+class suppressor:
+    def __init__(self, cam_name, v_type):
+        self.cam_name = cam_name
+        self.v_type = v_type
 
-def creator(cam_name,v_type,counter):
-    timenow = datetime.datetime.now()
-    timenow = int(timenow.strftime("%Y%m%d%H%M%S"))
-    with open(r'time.yaml') as rfile:
-        violation_list = yaml.load(rfile, Loader=yaml.FullLoader)
-    if counter == 1:
-        violation_list[cam_name][v_type] = timenow        
-    else:
-        dicts = {}
-        dicts[v_type] = timenow
-        violation_list[cam_name] = dicts
-    with open(r'time.yaml', 'w') as wfile:
-            documents = yaml.dump(violation_list, wfile)
-
-
-def checker(cam_name, v_type):
-    creation()
-    with open(r'time.yaml') as rfile:
-        violation_list = yaml.load(rfile, Loader=yaml.FullLoader)
-    try:
-        lastviolationtime = violation_list[cam_name]
+    def creation(self):
+        with open('time.yaml', 'r', encoding='utf-8') as rfile:
+            violation_list = yaml.load(rfile, Loader=yaml.FullLoader)
         try:
-            lastviolationtime = violation_list[cam_name][v_type]
-            timenow =  datetime.datetime.now()
-            timenow = int(timenow.strftime("%Y%m%d%H%M%S"))
-            threshold = violation_list['Time']
-            if (timenow-lastviolationtime)>threshold:
-                creator(cam_name,v_type,1)
-                return True
-            else:
+            len(violation_list)
+        except TypeError:
+            dict_file = {'Time' : 3600}
+            with open(r'time.yaml', 'w', encoding='utf-8') as wfile:
+                yaml.dump(dict_file, wfile)
+
+    def creator(self,counter):
+        timenow = datetime.datetime.now()
+        timenow = int(timenow.strftime("%Y%m%d%H%M%S"))
+        with open(r'time.yaml', 'r', encoding='utf-8') as rfile:
+            violation_list = yaml.load(rfile, Loader=yaml.FullLoader)
+        if counter == 1:
+            violation_list[self.cam_name][self.v_type] = timenow
+        else:
+            dicts = {}
+            dicts[self.v_type] = timenow
+            violation_list[self.cam_name] = dicts
+        with open(r'time.yaml', 'w', encoding='utf-8') as wfile:
+            yaml.dump(violation_list, wfile)
+
+    def checker(self):
+        self.creation()
+        with open(r'time.yaml', 'r', encoding='utf-8') as rfile:
+            violation_list = yaml.load(rfile, Loader=yaml.FullLoader)
+        try:
+            lastviolationtime = violation_list[self.cam_name]
+            try:
+                lastviolationtime = violation_list[self.cam_name][self.v_type]
+                timenow =  datetime.datetime.now()
+                timenow = int(timenow.strftime("%Y%m%d%H%M%S"))
+                threshold = violation_list['Time']
+                if (timenow-lastviolationtime)>threshold:
+                    self.creator(1)
+                    return True
                 return False
-        except Exception:
-            creator(cam_name,v_type,1)
+            except KeyError:
+                self.creator(1)
+                return True
+        except KeyError:
+            self.creator(0)
             return True
-    except Exception:
-        creator(cam_name,v_type,0)
-        return True
